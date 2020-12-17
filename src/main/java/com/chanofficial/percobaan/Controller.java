fd245c6661996371e5c011707adfaa3d6badf69a
fajri <fajririnaldichan@gmail.com>
2020-12-17 15:55:04 +0700

    implementasi push message1 d

--- src/main/java/com/chanofficial/percobaan/Controller.java
+++ src/main/java/com/chanofficial/percobaan/Controller.java
@@ -56,7 +56,7 @@ public class Controller {
                             replyText(messageEvent.getReplyToken(), event.getSource().getUserId());
                         }
                         if(textMessageContent.getText().equalsIgnoreCase("halo")){
-                            replyText(messageEvent.getReplyToken(), "halo anjeng");
+                            replyText(messageEvent.getReplyToken(), "halo kamu");
                         }
                         replyText(messageEvent.getReplyToken(), textMessageContent.getText());
                     }
